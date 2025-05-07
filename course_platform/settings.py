4b4b1fa8ed12b0a37c45d6f81024e0748d5e4db2
TheCamilovisk <camilolgon@gmail.com>
2025-05-07 12:52:58 -0300

    Handling media files.

--- course_platform/settings.py
+++ course_platform/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOCAL_CDN = BASE_DIR / 'local-cdn'
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = LOCAL_CDN / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
