b44707da70649222c2a998a67c6c8b4e5a5aade3
TheCamilovisk <camilolgon@gmail.com>
2025-05-08 11:31:29 -0300

    Ordering with a model field.

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
-from .models import Course
+from .models import Course, Lesson
 
-admin.site.register(Course)
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    readonly_fields = ['updated']
+    extra = 0
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ['title', 'status', 'access']
+    list_filter = ['status', 'access']
+    fields = [
+        'title',
+        'description',
+        'status',
+        'image',
+        'access',
+        'display_image',
+    ]
+    readonly_fields = ['display_image']
+
+    def display_image(self, obj, *args, **kwargs):
+        url = obj.image_admin
+        return format_html(f'<img src="{url}"')
+
+    display_image.short_description = 'Current Image'
+
+
+# admin.site.register(Course)
--- courses/models.py
+++ courses/models.py
@@ -40,3 +40,43 @@ class Course(models.Model):
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+
+    @property
+    def image_admin(self):
+        if not self.image:
+            return ''
+        image_options = {'width': 200}
+        return self.image.build_url(**image_options)
+
+    def get_image_detail(self, as_html=False, width=750):
+        if not self.image:
+            return ''
+        image_options = {'width': width}
+        if as_html:
+            return self.image.image(**image_options)
+        return self.image.build_url(**image_options)
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField('image', blank=True, null=True)
+    video = CloudinaryField(
+        'video', blank=True, null=True, resource_type='video'
+    )
+    order = models.IntegerField(default=0)
+    can_preview = models.BooleanField(
+        default=False,
+        help_text='If user does not have access to course, can they see it?',
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=PublishStatus.choices,
+        default=PublishStatus.PUBLISHED,
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', '-updated']
