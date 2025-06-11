b1a9aef0604c58fc8d5a23e76da53e9a48d5a519
TheCamilovisk <camilolgon@gmail.com>
2025-06-11 17:17:44 -0300

    Send Email.

--- course_platform/settings.py
+++ course_platform/settings.py
@@ -14,6 +14,22 @@ from pathlib import Path
 
 from decouple import config
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', cast=str, default=None)
+EMAIL_PORT = config('EMAIL_PORT', cast=str, default='587')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', cast=str, default=None)
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str, default=None)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+
+ADMIN_USER_NAME = config('ADMIN_USER_NAME', default='Admin user')
+ADMIN_USER_EMAIL = config('ADMIN_USER_EMAIL', default=None)
+
+MANAGERS = []
+ADMINS = []
+if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
+    ADMINS += [(f'{ADMIN_USER_NAME}', f'{ADMIN_USER_EMAIL}')]
+    MANAGERS = ADMINS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOCAL_CDN = BASE_DIR / 'local-cdn'
